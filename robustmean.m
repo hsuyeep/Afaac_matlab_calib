b2d72e47e4de639e02c64f407047c76925d16a85
Peeyush Prasad <p.prasad@uva.nl>
2014-02-11 14:44:38 +0100

    Corrected robustmean.m to take care of NaNs in data, updated pelican_tracking_cal.m totake WSF solutions from a previous timeslice. 	modified:   pelican_tracking_cal.m 	modified:   robustmean.m

--- robustmean.m
+++ robustmean.m
@@ -18,27 +18,30 @@ function [m, v, sel] = robustmean (x, sig)
 		v = 0;
 	end;
 
+	sel = ones (length (x), 1);
+	nandat = sel;
+	nandat (isnan (x) == 1) = 0; % list of nan values in data
+
 	if (isreal(x) == 1)
-		sel = ones (1, length (x));
 		for ind = 1:10
+			sel = (sel & nandat); 
 			m1 = mean (x(sel == 1));
 			v1 = std (x(sel == 1));
 			sel = (abs (x - m1) / v1) < sig;
 		end;	
 		m = m1; v = v1;
 	else
-		sel = ones (1, length (x));
 		selr = sel; seli = sel;
 		for ind = 1:10
+			sel = selr & seli & nandat; 
 			mr1 = mean (real(x(sel == 1)));
 			mi1 = mean (imag(x(sel == 1)));
 
 			vr1 = std (real(x(sel == 1)));
 			vi1 = std (imag(x(sel == 1)));
 			selr = (abs (real(x) - mr1) / vr1) < sig;
-			seli = (abs (real(x) - mi1) / vi1) < sig;
+			seli = (abs (imag(x) - mi1) / vi1) < sig;
 			% We reject if any of real/imag breaks threshold.
-			sel = selr & seli; 
 		end;	
 		m = mr1 + 1*i*mi1;
 		v = vr1 + 1*i*vi1;
