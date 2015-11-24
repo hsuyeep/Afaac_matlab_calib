3af78e74b335a3ea08c34bfbabaceeb55e0a88af
Peeyush Prasad <prasad@astron.nl>
2015-11-24 12:47:25 +0100

    copied gencalflux from laptop to allow pull on laptop

--- gencalflux.m
+++ gencalflux.m
@@ -3,15 +3,20 @@
 % Arguments:
 %   srcname :   Name of calibrator whose flux is required.
 %   freq    :   Frequency (Hz) at which the flux is required.
-
 function flux = gencalflux (srcname, freq)
     % Fit parameters from Scaife's model fits
-    fitparms = containers.Map ({'3C48', '3C147', '3C196', '3C286'}, { [ 64.768  -0.387  -0.420   0.181], [ 66.738  -0.022  -1.012   0.549], [ 83.084  -0.699  -0.110   0    ], [ 27.477  -0.158   0.032  -0.180] });
+    fitparms = containers.Map (
+        {'3C48', '3C147', '3C196', '3C286', '3C295'},
+        { [ 64.768  -0.387  -0.420   0.181],    % 3C48
+          [ 66.738  -0.022  -1.012   0.549],    % 3C147
+          [ 83.084  -0.699  -0.110   0    ],    % 3C196
+          [ 27.477  -0.158   0.032  -0.180]     % 3C286
+        });
 
-    assert (fitparms.isKey(upper (srcname)));
+    assert (contains (fitparms, lower (srcname)));
 
     % Model specification
     coe = log10(freq/150e6);
-    mod = [1 coe  coe^2 coe^3];
+    mod = [1 coe    coe^2 coe^3];
 
-    flux = fitparms(upper(srcname)).*mod;
+    flux = mod*fitparms(lower(srcname));
